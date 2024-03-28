19f1b0deea000242db16a18067ad6af8635d75c6
Dshushu73 <164135301+Dshushu73@users.noreply.github.com>
2024-03-27 21:32:40 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    var coll = document.getElementsByClassName("note-btn");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
+    var subColl = document.getElementsByClassName("sub-note-btn");
+    for (i = 0; i < subColl.length; i++) {
+        subColl[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+});
